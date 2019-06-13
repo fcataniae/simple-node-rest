09b32149ec6a6aada95b9fc55ea6a13f6519dffb
fcataniae <cataniafrane@gmail.com>
2019-06-12 21:07:04 -0300

    add rabbit mq integration, refactor of structure. TODO make configuration file

--- app/app.js
+++ app/app.js
@@ -1,7 +1,11 @@
 const Express = require("express");
 const bodyparser = require('body-parser');
 const querystring = require('querystring');
+const Utils = require('./rabbitmq/utils.js');
+const rabbitools = new Utils();
 const app = Express();
+
+rabbitools.channel =  rabbitools.init();
 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
@@ -11,20 +15,19 @@ app.get('/', function(req, res){
   res.send('consuming main page');
 });
 
-app.get('/home', function(req, res){
-  let query = req.query.data;
-  let query2 = req.query.date;
-  console.log('query data  = ' + query + ' querty date = ' + query2);
-  res.send('consuming /home page');
-});
-
 app.route('/usuario')
   .get(function(req,res){
+    let query = req.query.data;
+    let query2 = req.query.date;
+    console.log('query data  = ' + query + ' querty date = ' + query2);
     res.send('respuesta desde usuario.get');
   })
   .post(function(req,res){
+    console.log(req.body);
+    rabbitools.uploadToQueue("queueExpressJs",Buffer.from(JSON.stringify(req.body)));
     res.send('respuesta desde usuario.post');
   });
+
 app.use(function(req, res, next) {
  respuesta = {
   error: true,
---
+++ app/rabbitmq/utils.js
@@ -0,0 +1,26 @@
+module.exports = function (){
+
+  const amqplib = require('amqplib');
+  this.channel = null;
+
+  this.init = function(){
+    return require('amqplib').connect('amqp://localhost')
+        .then(conn => conn.createChannel())
+        .then(ch => {
+            channel = ch;
+
+            //this queue is a "Direct reply-to" read more at the docs
+            //When some msg comes in, we "emit" a message to the proper "correlationId" listener
+            ch.consume('amq.rabbitmq.reply-to', msg => eventEmitter.emit(msg.properties.correlationId, msg.content), {noAck: true});
+        });
+  }
+
+  this.uploadToQueue = function(queueName,toUpload){
+
+    channel.assertQueue(queueName)
+        //Sent the buffered img to the queue with the ID and the responseQueue
+        .then(() => channel.sendToQueue(queueName, toUpload, {replyTo: 'amq.rabbitmq.reply-to'}));
+
+  }
+
+}
